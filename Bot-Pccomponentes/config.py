184cf37407dcb9e3e724af8aacf3754fc8b5b795
diaverso <alexito_perez.95@hotmail.com>
2020-11-02 19:10:43 +0100

    Actualizacion y modificacion

---
+++ Bot-Pccomponentes/config.py
@@ -0,0 +1,5 @@
+keys = {
+    "product_url": "",
+    "email": "",
+    "pass": ""
+}
--- config.py
+++
@@ -1,5 +0,0 @@
-keys = {
-    "product_url": "https://www.pccomponentes.com/amd-ryzen-threadripper-3990x",
-    "email": "alexito_perez.95@hotmail.es",
-    "pass": "a1234567"
-}
